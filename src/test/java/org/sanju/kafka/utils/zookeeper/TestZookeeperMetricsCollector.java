576b9114924a6806e6bf1748b3065c64a0f14353
Thomas <sathomas@ct1-8780-m.home>
2018-01-13 23:39:42 -0500

    adding more code

---
+++ src/test/java/org/sanju/kafka/utils/zookeeper/TestZookeeperMetricsCollector.java
@@ -0,0 +1,18 @@
+package org.sanju.kafka.utils.zookeeper;
+
+import org.junit.Before;
+import org.sanju.kafka.utils.broker.AbstractTest;
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
+public class TestZookeeperMetricsCollector extends AbstractTest {
+
+	@Before
+	public void setup() {
+		super.setup();
+	}
+
+}
