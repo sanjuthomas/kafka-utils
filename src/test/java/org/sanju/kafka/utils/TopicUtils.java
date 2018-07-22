527c8eea32bc9e3fe757344e1a778abaaa6eb1ab
sanjuthomas <sanju@sanju.org>
2018-07-22 17:42:24 -0400

    adding more code

--- src/test/java/org/sanju/kafka/utils/TopicUtils.java
+++ src/test/java/org/sanju/kafka/utils/TopicUtils.java
@@ -5,6 +5,11 @@ import java.util.Set;
 
 import kafka.admin.RackAwareMode;
 
+/**
+ *
+ * @author Sanju Thomas
+ *
+ */
 public class TopicUtils extends AdminUtils {
 
 	public TopicUtils(final String kafkaHost, final String zooKeeperHost) {
@@ -12,7 +17,7 @@ public class TopicUtils extends AdminUtils {
 	}
 
 	public void createTopic(final String topic) {
-		kafka.admin.AdminUtils.createTopic(this.zkUtils(), topic, 1, 1, new Properties(),
+		kafka.admin.AdminUtils.createTopic(super.zkUtils(), topic, 1, 1, new Properties(),
 				RackAwareMode.Enforced$.MODULE$);
 	}
 
@@ -20,4 +25,11 @@ public class TopicUtils extends AdminUtils {
 		topics.forEach(t -> this.createTopic(t));
 	}
 
+	public void deleteTopcis(final Set<String> topics) {
+		topics.forEach(t -> this.deleteTopic(t));
+	}
+
+	public void deleteTopic(final String topic) {
+		kafka.admin.AdminUtils.deleteTopic(super.zkUtils(), topic);
+	}
 }
